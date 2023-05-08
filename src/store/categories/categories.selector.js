1a8a7e0bc92441106e98e05a7d643408abab019d
s-mcmartin <shannoncmcguire@outlook.com>
2023-05-08 11:57:10 -0400

    Add reselect

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,10 +1,23 @@
-export const selectCategoriesMap = (state) => {
-  const categoriesMap = state.categories.categories.reduce(
-    (acc, { title, items }) => {
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => {
+  return state.categories;
+};
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => {
+    return categoriesSlice.categories;
+  }
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    },
-    {}
-  );
-  return categoriesMap;
-};
+    }, []);
+  }
+);
